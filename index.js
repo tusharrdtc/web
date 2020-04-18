527eeca6dc86f29d74c9cc35f4366ac8899371ba
tusharrdtc <tysocial1@gmail.com>
2020-04-18 19:08:21 +0530

    added html file

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const http = require('http');
+var express = require('express');
+var app = express();
 
-const PORT = process.env.PORT || 5000;
+var port = process.env.PORT || 8080
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.render("index");
-});
+app.use(express.static(__dirname + "/"));
+app.set('view engine', 'html');
 
-server.listen(PORT, () => {
-  console.log(`Server running on ${PORT}/`);
-});
+app.get("/", function(req, res){
+    res.render('index');
+})
+
+app.listen(port, function(){
+    console.log("app running");
+})
